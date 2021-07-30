3a63f110275365b7575f3bd4646cdb140e124db5
EmanuelVolpe <emanuelvolpe@gmail.com>
2021-07-30 00:49:03 -0300

    agrega mejoras visuales. NO ANDA bien el session

---
+++ mvc_v1/templates_c/10dc86562ae1729d06d91cd4482ffcddef5a5ac4_0.file.showErrorLogin.tpl.php
@@ -0,0 +1,30 @@
+<?php
+/* Smarty version 3.1.39, created on 2021-07-28 22:20:15
+  from 'C:\xampp\htdocs\crudTudai\mvc_v1\templates\showErrorLogin.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_6101bbffecf534_55204013',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '10dc86562ae1729d06d91cd4482ffcddef5a5ac4' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\crudTudai\\mvc_v1\\templates\\showErrorLogin.tpl',
+      1 => 1627503423,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6101bbffecf534_55204013 (Smarty_Internal_Template $_smarty_tpl) {
+?><div class="container mt-5">
+    <div class="jumbotron">
+        <h1 class="display-4"><?php echo $_smarty_tpl->tpl_vars['msgDeError']->value;?>
+</h1>
+    </div>
+</div><?php }
+}
